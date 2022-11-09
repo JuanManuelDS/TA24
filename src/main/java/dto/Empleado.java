6582841d3279d45e2b87dccca605913b73d9f894
JuanManuelDS <juanmadesenzi@gmail.com>
2022-11-09 10:23:40 +0100

    dto finalizado

--- src/main/java/dto/Empleado.java
+++ src/main/java/dto/Empleado.java
@@ -1,5 +1,61 @@
 package dto;
 
-public class Empleado {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table (name="empleados") //Por si la tabla es diferente
+public class Empleado {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "nombre") //Es para recordar, si es igual no hace falta
+	private String nombre;
+	@Column
+	private String trabajo;
+	@Column
+	private int salario;
+	
+	
+	public Empleado() {
+		
+	}
+	
+	public Empleado(Long id, String nombre, String trabajo, int salario) {
+		this.id = id;
+		this.nombre = nombre;
+		this.trabajo = trabajo;
+		this.salario = salario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTrabajo() {
+		return trabajo;
+	}
+	public void setTrabajo(String trabajo) {
+		this.trabajo = trabajo;
+	}
+	public int getSalario() {
+		return salario;
+	}
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+	public Long getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Empleado [id=" + id + ", nombre=" + nombre + ", trabajo=" + trabajo + ", salario=" + salario + "]";
+	}
+	
+	
 }
