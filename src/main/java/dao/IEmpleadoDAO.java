81955d386487dabeed2f3bdb6afcddffee16530a
JuanManuelDS <juanmadesenzi@gmail.com>
2022-11-09 10:35:31 +0100

    dao dev terminado

--- src/main/java/dao/IEmpleadoDAO.java
+++ src/main/java/dao/IEmpleadoDAO.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dto.Empleado;
 
 //JpaRepository <Empleado, Long> hace referencia a la table empleado y su clave primaria
-public interface ClienteDAO extends JpaRepository<Empleado, Long> {
+public interface IEmpleadoDAO extends JpaRepository<Empleado, Long> {
 	public List<Empleado> findByNombre(String nombre);
 }
