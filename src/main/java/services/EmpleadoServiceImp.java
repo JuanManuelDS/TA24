1a103be686403b51bd348e9f639daffecfcd67c7
JuanManuelDS <juanmadesenzi@gmail.com>
2022-11-09 10:50:05 +0100

    services terminado

--- src/main/java/services/EmpleadoServiceImp.java
+++ src/main/java/services/EmpleadoServiceImp.java
@@ -1,5 +1,55 @@
 package services;
 
-public class EmpleadoServiceImp {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.IEmpleadoDAO;
+import dto.Empleado;
+
+public class EmpleadoServiceImp implements IntEmpleadoService{
+	//Esto nos permite utilizar los métodos de la interface IEmpleadoDAO. Como si la instanciaramos
+	@Autowired
+	IEmpleadoDAO iEmpleadoDAO;
+
+	@Override
+	public List<Empleado> listarEmpleados() {
+		
+		return iEmpleadoDAO.findAll();
+	}
+
+	@Override
+	public Empleado guardarEmpleado(Empleado empleado) {
+		
+		return iEmpleadoDAO.save(empleado);
+	}
+
+	@Override
+	public Empleado buscarEmpleado(Long id) {
+		
+		return iEmpleadoDAO.findById(id).get();
+	}
+
+	@Override
+	public Empleado actualizarEmpleado(Empleado empleado) {
+		
+		return iEmpleadoDAO.save(empleado);
+	}
+
+	@Override
+	public void eliminarEmpleado(Long id) {
+		
+		iEmpleadoDAO.deleteById(id);
+	}
+	
+	@Override
+	public List<Empleado> listarEmpleadosPorNombre(String nombre) {
+		
+		return iEmpleadoDAO.findByNombre(nombre);
+	}
+
+	
+	
+	
 }
--- src/main/java/services/IntEmpleadoService.java
+++ src/main/java/services/IntEmpleadoService.java
@@ -1,5 +1,21 @@
 package services;
 
+import dto.Empleado;
+import java.util.List;
+
+//Estos son los métodos que se podrán realizar sobre la tabla empleados
 public interface IntEmpleadoService {
 
+	public List<Empleado> listarEmpleados();
+	
+	public Empleado guardarEmpleado(Empleado empleado);
+	
+	public Empleado buscarEmpleado(Long id);
+	
+	public List<Empleado> listarEmpleadosPorNombre(String nombre);
+	
+	public Empleado actualizarEmpleado(Empleado empleado);
+	
+	public void eliminarEmpleado(Long id);
+	
 }
